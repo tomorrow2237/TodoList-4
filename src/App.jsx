7c6527bba29ae22cc67664b51746d911602fac50
tomorrow2237 <tomokichimarch@gmail.com>
2024-02-23 17:35:12 +0900

    second-version

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
+import { useState } from "react"
+
 function App() {
+
+  const Todos = [
+    { id:1, task: "Shopping"},
+    { id:2, task: "Running"},
+    { id:3, task: "Washing my car"}
+  ]
+  const [jobs, setJobs] = useState(Todos);
+
   return (
     <>
+    <h1>Symplized Todo List</h1>
+      {jobs.map((job) => {
+        <h4>{job.task}</h4>
+        })}
     </>
   )
 }
